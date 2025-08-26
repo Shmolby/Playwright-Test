b56a22bd299513465059a87cce11901acec07879
Wouter <wjkjiva@gmail.com>
2025-08-26 20:08:43 +0200

    Edited login and products so they work, started on cart

--- pages/login.py
+++ pages/login.py
@@ -3,16 +3,15 @@ from playwright.sync_api import Page
 
 class LoginPage:
 
-    URL = "https://www.saucedemo.com/"
-
     def __init__(self, page: Page):
-        self.page = Page
+        self.page = page
+        self.url = 'https://www.saucedemo.com/'
         self.username_field = page.locator("xpath=//input[@data-test='username']")
         self.password_field = page.locator("xpath=//input[@data-test='password']")
         self.login_button = page.locator("xpath=//input[@data-test='login-button']")
 
     def load(self):
-        self.page.goto(self.URL)
+        self.page.goto(self.url)
 
     def login(self, username: str, password: str):
         self.username_field.fill(username)
--- pages/products.py
+++ pages/products.py
@@ -3,24 +3,25 @@ from playwright.sync_api import Page
 
 class ProductsPage:
 
-    URL = "https://www.saucedemo.com/inventory.html"
-
     def __init__(self, page: Page):
         self.page = page
+        self.url = "https://www.saucedemo.com/inventory.html"
         self.inventory_item = page.locator("xpath=//div[@data-test='inventory-item-name']")
+        self.shopping_cart = page.locator("xpath=//a[@data-test='shopping-cart-link']")
 
     def load(self):
-        self.page.goto(self.URL)
+        self.page.goto(self.url)
 
     def products(self) -> list[str]:
-        self.inventory_item.wait_for()
-        return self.inventory_item.all_text_contents()
+        product_list = self.inventory_item.all()
+        product_names = []
+        for item in product_list:
+            product_names.append(item.text_content())
+        return product_names
 
     def add_product_to_cart(self, product: str):
-        all_products = self.products()
-        product_name = [item for item in all_products if (product.lower() == item.lower())]
-        self.page.locator(f"xpath=//div[@data-test='inventory-item-name' and text() = '{product_name}']//ancestor::div[@class = 'inventory_item_description']//button[text() = 'Add to cart']").click()
+        self.page.locator(f"xpath=//div[@data-test='inventory-item-name' and text() = '{product}']//ancestor::div[@class = 'inventory_item_description']//button[text() = 'Add to cart']").click()
 
     def go_to_checkout(self):
-        self.page.locator("xpath=//")
+        self.page.locator("xpath=//a[@data-test='shopping-cart-link']").click()
 
